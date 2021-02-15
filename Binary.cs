5b1592d5684f16cecf3876d84f8270309774e30d
Vinícius Neto <57202563+viunow@users.noreply.github.com>
2021-02-15 15:33:45 -0300

    Add files via upload

---
+++ Binary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Binarios
+{
+    public class Binary
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Binary from 0 to 255");
+            Console.WriteLine();
+
+            int counter = 0;
+
+            while (counter <= 255)
+            {
+                string binary = Convert.ToString(counter, 2);
+                Console.WriteLine($"{counter} => {binary}");
+                counter++;
+            }
+        }
+
+    }
+}
